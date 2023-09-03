b9e218053beb5343cfa8835005184758b1945be9
smithwithatypo <smithwithatypo@gmail.com>
2023-09-02 23:44:35 -0500

    initial commit, added form

---
+++ src/app/input-form/input-form.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-input-form',
+  templateUrl: './input-form.component.html',
+  styleUrls: ['./input-form.component.css']
+})
+export class InputFormComponent {
+  projectForm!: FormGroup;
+  projects_list: any = [];
+
+  constructor(private fb: FormBuilder) { 
+    
+  }
+
+  ngOnInit(): void {
+    this.projectForm = this.fb.group({
+      project_name: ['', Validators.required],
+      importance: [0, [Validators.required, Validators.min(0), Validators.max(20)]],
+      urgency: [0, [Validators.required, Validators.min(0), Validators.max(20)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.projectForm.valid) {
+      const formValues = this.projectForm.value;
+      // console.log(formValues);
+      this.projects_list.push(formValues);
+      console.log(this.projects_list);
+    }
+  }
+}
